9a2e48136e2eac6affa5b71ea8520c729d5a2878
james-mumo <mumojames98@gmail.com>
2023-08-18 12:35:51 +0300

    latest ML commit

--- py/ds/hashmap.py
+++
@@ -1,9 +0,0 @@
-# hashmap hold in table such is key and val
-# key can id and val is name
-# has functions
-
-# we have key value pair
-
-# we can use mmydict = dict() or {"key":"val"}
-
-# print mydict
--- py/ds/readme.py
+++
@@ -1,9 +0,0 @@
-# in python we have lists
-# store multiple vars together
-# include tuple, list, set and dic
-
-# tuple haves brackets, ordered and unchangeable
-myTuple = ("abc", "dog", "aaapl")
-print(myTuple)
-
-# 
