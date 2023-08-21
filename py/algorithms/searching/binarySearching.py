e52d60d6d7429449442a99406700b4a9dbfba47c
james-mumo <mumojames98@gmail.com>
2023-08-21 18:02:27 +0300

    new

--- py/algorithms/searching/binarySearching.py
+++ py/algorithms/searching/binarySearching.py
@@ -21,22 +21,20 @@ def itBinSearch(arr, target):
 
 print(itBinSearch(arr, target))
 
+def recBinSearch(arr, key, l, r):
+    while l<= r:
+        mid = l + (r-l)//2
 
-def itBin(arr, t):
-    l = 0
-    r = len(arr)-1
-    
-    while l<=r:
-        mid = l + (r - l)//2
-        if arr[mid] == t:
+        # if found return it
+        if arr[mid] ==  key:
             return mid
-        elif arr[mid] < t:
-            l = mid + 1
+        elif arr[mid]<key:
+            return recBinSearch(arr, key, mid+1, r)
         else:
-            r = mid -1
+            return recBinSearch(arr, key, 0, mid-1)
     return -1
-    
-arrayF = [1,2,34,5,6,3]
-sortedF =  sorted(arrayF)
 
-print(itBin(sortedF, 3))
+
+rec_arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+key = 72
+print(recBinSearch(rec_arr, key, 0, len(rec_arr)-1))
