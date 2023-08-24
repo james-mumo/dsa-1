2d945a5adc6cc4f265ad9ffe4b50b1dfc7076070
james-mumo <mumojames98@gmail.com>
2023-08-24 18:43:15 +0300

    llists addition

--- py/ds/singlyllist.py
+++ py/ds/singlyllist.py
@@ -1,26 +1,20 @@
-# we have a node class with data and ref
+# we create the node
 class Node:
-    def __init__(self, data):
-        self.ref = None
+    def __init__(self, data="None", ref="None"):
         self.data = data
-
-# here is a head node of the chain of nodes
-node1 = Node(10)
+        self.ref = ref
 
 class LList:
     def __init__(self):
-        self.head = None    # since its empty here no head
+        self.head = None
 
-    def printList(self):
+    def insertBeginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def printme(self):
         if self.head is None:
-            print("LList is none")
-        else:
-            n = self.head
-            while n is not None:
-                print(n.data)
-                n = n.ref
-
-ll = LList()
-ll.printList()
+            print("List is empty")
+            return
 
 
