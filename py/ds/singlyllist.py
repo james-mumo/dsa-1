4b6e15a958ca1bf9e713fa3d5e7ec4cdbfc7b57a
james-mumo <mumojames98@gmail.com>
2023-08-24 19:54:09 +0300

    llists addition

--- py/ds/singlyllist.py
+++ py/ds/singlyllist.py
@@ -4,19 +4,29 @@ class Node:
         self.data = data
         self.ref = ref
 
+
 class LList:
     def __init__(self):
         self.head = None
 
-    def insertBeginning(self, data):
-        node = Node(data, self.head)
-        self.head = node
-
     def printme(self):
         if self.head is None:
             print("List is empty")
-            return
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data)
+                n = n.ref
+
+    def add_beginning(self, data):
+        newNode = Node(data)
+        newNode.ref = self.head
+        self.head = newNode
 
 
-
-
+ll = LList()
+ll.add_beginning(12)
+ll.add_beginning(12)
+ll.add_beginning(22)
+ll.add_beginning(233)
+ll.printme()
