d05eb6ce3c0b6f927fe4416ae43b5df9bc2f0f6f
james-mumo <mumojames98@gmail.com>
2023-10-19 18:38:03 +0300

    ml

--- py/ds/arrays.py
+++ py/ds/arrays.py
@@ -1,14 +1,13 @@
-import array #module array so we can have the apend and insert and pop functons
+import array  # module array so we can have the append and insert and pop functons
 
-arr = array.array("i", [1,2,3]) # an array of integers and initialized
+arr = array.array("i", [1, 2, 3])  # an array of integers and initialized
 
 # arr.append(4)
 # arr.clear()
-arr.extend([5,4,8,9,6])
+arr.extend([5, 4, 8, 9, 6])
 print(arr.__contains__(1))
 arr = sorted(arr)
 
 # loop over the array elements
 for i in range(0, len(arr)):
     print(arr[i])
-
