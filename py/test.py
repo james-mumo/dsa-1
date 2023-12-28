da6322de7635df5f5a6234e2fb3e509bc409b3d8
james-mumo <mumojames98@gmail.com>
2023-12-28 07:44:57 +0300

    new

--- py/test.py
+++ py/test.py
@@ -1,4 +1,20 @@
-arr = [1,2,4,5,6,]
+# for loop
 
-for i in arr:
-    print(i)
+for i in range(33):
+    if i == 32:
+        break
+    # print(i)
+
+# linear time loop
+arr = [1, 2, 4, 5, 6, ]
+
+# for i in arr:
+# print(i)
+
+
+# nested Loop with n^2
+arr1 = [[1, 2, 3], [5, 5, 6]]
+
+for i in range(len(arr1)):
+    for j in range(len(arr1[i])):
+        print(arr1[i][j])
