00448072f8e4951d0219bb68bc512a797c5beed8
james-mumo <mumojames98@gmail.com>
2024-01-04 10:04:50 +0300

    new changes

--- py/neetcode/arrays_hashing/3Sum.py
+++ py/neetcode/arrays_hashing/3Sum.py
@@ -1,2 +1,11 @@
 # def ThreeSum(A, arr_size, sum):
-#
+def tsum(nums, key):
+    res = []
+    nums.sort()
+
+    for k, v in enumerate(nums):
+        if k > 0 and v == nums[v - 1]:
+            continue
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            threeSum = a +
